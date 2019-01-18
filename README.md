33ca1d1dde0b361f1e7d265c187581e876e2eef6
dawnmarch <wjqxhy@qq.com>
2019-01-18 17:56:48 +0800

    add meteorlxy and add config

---
+++ README.md
@@ -0,0 +1,14 @@
+# [dawnmarch.github.io](http://dawnmarch.github.io)
+
+My Homepage Repo
+
+Powered by [Vuepress](https://vuepress.vuejs.org/)
+
+Themed by [vuepress-theme-meteorlxy](https://github.com/meteorlxy/vuepress-theme-meteorlxy)
+
+## Develop
+
+```
+yarn
+yarn docs:dev
+```
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,78 @@
+module.exports = {
+    title: 'DawnMarch',
+
+    description: 'DawnMarch\'s homepage',
+
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: '/assets/img/avator.jpg'
+        }],
+    ],
+
+    locales: {
+        '/': {
+            lang: 'zh-CN',
+        },
+    },
+
+    // use this theme
+    theme: 'meteorlxy',
+
+    // set theme config
+    themeConfig: {
+        personalInfo: {
+            // your personal info to display in the info card
+            nickname: 'DawnMarch',
+            description: 'Happy Coding<br/>Happy Life',
+            email: 'wjqxhy@qq.com',
+            location: 'ShenZhen City, China',
+            organization: 'Hunan University',
+
+            // your avator image to display in the info card (related to the .vuepress/public directory)
+            avator: '/assets/img/avator.jpg',
+
+            // your sns accounts to display in the page footer and the bottom of info card
+            sns: {
+                github: {
+                    account: 'dawnmarch',
+                    link: 'https://github.com/dawnmarch'
+                },
+                weibo: {
+                    account: '@破晓三月',
+                    link: 'https://weibo.com/'
+                }
+            }
+        },
+
+        // background of the header
+        headerBackground: {
+            // set url for image background (related to the .vuepress/public directory)
+            // url: '/assets/img/bg.jpg',
+
+            // use geopattern to generate background randomly
+            useGeo: true
+        },
+
+        // use the official last-update plugin
+        lastUpdated: true,
+
+        // for the navbar
+        nav: [{
+                text: 'Home',
+                link: '/',
+                exact: true
+            },
+            {
+                text: 'Posts',
+                link: '/posts/',
+                exact: false
+            },
+            {
+                text: 'About',
+                link: '/about/',
+                exact: false
+            },
+        ]
+    },
+}
