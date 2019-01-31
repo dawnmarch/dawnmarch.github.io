4b0c1ee8172a9e4e8fa1b12d9c103f219e382caa
dawnmarch <wjqxhy@qq.com>
2019-01-31 11:37:57 +0800

    format

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -77,29 +77,29 @@ module.exports = {
     },
     markdown: {
         toc: {
-          includeLevel: [2, 3, 4]
+            includeLevel: [2, 3, 4]
         }
-      },
-      chainWebpack: (config, isServer) => {
+    },
+    chainWebpack: (config, isServer) => {
         if (isServer === false) {
-          config.node.set('Buffer', false)
-    
-          config.optimization.splitChunks({
-            maxInitialRequests: 5,
-            cacheGroups: {
-              vue: {
-                test: /[\\/]node_modules[\\/](vue|vue-router)[\\/]/,
-                name: 'vendor.vue',
-                chunks: 'all'
-              },
-              commons: {
-                test: /[\\/]node_modules[\\/]/,
-                priority: -10,
-                name: 'vendor.commons',
-                chunks: 'all'
-              }
-            }
-          })
+            config.node.set('Buffer', false)
+
+            config.optimization.splitChunks({
+                maxInitialRequests: 5,
+                cacheGroups: {
+                    vue: {
+                        test: /[\\/]node_modules[\\/](vue|vue-router)[\\/]/,
+                        name: 'vendor.vue',
+                        chunks: 'all'
+                    },
+                    commons: {
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: -10,
+                        name: 'vendor.commons',
+                        chunks: 'all'
+                    }
+                }
+            })
         }
     }
 }
